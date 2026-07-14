500a7a60cededb7bc619e548734a1800f5b20c42
hsh6883 <hsh031022@gmail.com>
2026-07-14 17:25:13 +0900

    Update __init__.py

--- adapters/MA-PDM/datasets/__init__.py
+++ adapters/MA-PDM/datasets/__init__.py
@@ -4,18 +4,20 @@ from .military import MilitaryBoundaryDataset
 def get_dataset(args, config):
     dataset_name = config.data.dataset
     data_root = config.data.data_dir
-    
+
     if dataset_name.lower() == 'military':
         train_dataset = MilitaryBoundaryDataset(
-            data_root=data_root, 
+            data_root=data_root,
+            phase="train",
             resize_shape=(config.data.image_size, config.data.image_size),
-            frame_length=config.data.time_step + 1,  
+            frame_length=config.data.time_step + 1,
             patch_size=config.data.patch_size
         )
         test_dataset = MilitaryBoundaryDataset(
-            data_root=data_root, 
+            data_root=data_root,
+            phase="test",
             resize_shape=(config.data.image_size, config.data.image_size),
-            frame_length=config.data.time_step + 1,  
+            frame_length=config.data.time_step + 1,
             patch_size=config.data.patch_size
         )
         return train_dataset, test_dataset
